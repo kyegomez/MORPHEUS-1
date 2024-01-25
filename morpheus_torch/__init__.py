3804479659c348c81dcbc14e213b3a8e5c8d3d5a
Kye <kye@apacmediasolutions.com>
2024-01-25 13:16:55 -0500

    [V]

--- morpheus_torch/__init__.py
+++ morpheus_torch/__init__.py
@@ -2,10 +2,12 @@ from morpheus_torch.model import (
     EEGConvEmbeddings,
     FMRIEmbedding,
     MorpheusEncoder,
+    MorpheusDecoder,
 )
 
 __all__ = [
     "EEGConvEmbeddings",
     "FMRIEmbedding",
     "MorpheusEncoder",
+    "MorpheusDecoder",
 ]
