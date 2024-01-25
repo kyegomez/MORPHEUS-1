90eea2705ad366df9efec7a05602330ed230b459
Kye <kye@apacmediasolutions.com>
2024-01-25 10:34:45 -0500

    [SCAFFOLD]

---
+++ morpheus1/model.py
@@ -0,0 +1,10 @@
+import torch 
+from torch import nn, Tensor
+from typing import List, Tuple
+
+class MorpheusTransformer(nn.Module):
+    def __init__(self):
+        pass
+    
+    def forward(self, x: Tensor):
+        pass
